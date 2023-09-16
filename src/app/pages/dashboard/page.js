8d191ce2333123dddf4aaf3d44e9c177caf53635
Betrizav <133694594+Betrizav@users.noreply.github.com>
2023-09-16 18:32:37 -0400

    Update page.js

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,10 +1,17 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
+import { Suspense } from "react";
+
 
 export default async function Dashboard() {
    
+    const users = getUsers()
     return (
         <div>
-            <h1>Dashboard</h1>
+
+          <Suspense fallback={<p> carregando pagina... </p>}>
+            <button><a href="/pages/alterar">lterar</a></button>
+            <button><a href="/pages/registro">Registrar</a></button>
+          </Suspense>
             
         </div>
     );
