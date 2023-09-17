a8b62a1950dc4af57b2198f3cec333efefd4efda
Betrizav <133694594+Betrizav@users.noreply.github.com>
2023-09-17 18:41:11 -0400

    Update page.js

--- src/app/pages/Registro/page.js
+++ src/app/pages/Registro/page.js
@@ -20,8 +20,8 @@ const Form = () => {
         <input placeholder='E-mail' type='email'></input>
         <input placeholder='Senha' type='password'></input>
 
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" ><a href="/pages/dashboard">Entrar</a></button>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"><a href="/pages/dashboard">Voltar</a></button>
+        <button><a href="/pages/dashboard">Entrar</a></button>
+        <button><a href="/pages/dashboard">Voltar</a></button>
         </form>
         <ToastContainer/>
     </div>
