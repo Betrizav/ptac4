29d51a98f0200679538cd32df2f3b863eb074c05
Betrizav <133694594+Betrizav@users.noreply.github.com>
2023-09-17 19:24:59 -0400

    Update page.js

--- src/app/pages/Registro/page.js
+++ src/app/pages/Registro/page.js
@@ -10,7 +10,7 @@ const Form = () => {
         toast.success('registrado com sucesso')
     }
    return (
-    <div>
+    <div className="container">
         
         <h1>Novo Registro</h1>
 
@@ -20,8 +20,8 @@ const Form = () => {
         <input placeholder='E-mail' type='email'></input>
         <input placeholder='Senha' type='password'></input>
 
-        <button><a href="/pages/dashboard">Entrar</a></button>
-        <button><a href="/pages/dashboard">Voltar</a></button>
+        <button className="botaozinho"><a href="/pages/dashboard">Entrar</a></button>
+        <button className="botaozinho"><a href="/pages/dashboard">Voltar</a></button>
         </form>
         <ToastContainer/>
     </div>
