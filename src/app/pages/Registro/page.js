107bff7a844d4d3628abbd59e7d4758361dec6a6
Betrizav <133694594+Betrizav@users.noreply.github.com>
2023-09-17 01:24:01 -0400

    Update page.js

--- src/app/pages/Registro/page.js
+++ src/app/pages/Registro/page.js
@@ -11,14 +11,17 @@ const Form = () => {
     }
    return (
     <div>
-        <h1>Login</h1>
-        <form onSubmit={handler}>
+        
+        <h1>Novo Registro</h1>
+
+        <form onSubmit={handlerLogin}>
            
         <input placeholder='nome' type='nome'></input>
         <input placeholder='E-mail' type='email'></input>
         <input placeholder='Senha' type='password'></input>
 
-        <button>Entrar</button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" ><a href="/pages/dashboard">Entrar</a></button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"><a href="/pages/dashboard">Voltar</a></button>
         </form>
         <ToastContainer/>
     </div>
