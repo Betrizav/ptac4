793c55b73d679f855ce8ce724d1014c0e2315c4a
Betrizav <133694594+Betrizav@users.noreply.github.com>
2023-09-18 20:51:24 -0400

    Update Page.js

--- src/app/pages/Alterar/Page.js
+++ src/app/pages/Alterar/Page.js
@@ -9,16 +9,17 @@ const Form = () => {
         toast.success('alterado com sucesso')
     }
     return (
-        <div className='container'>
-           <h1>Alterar dados</h1>
+        <div className='containerdash'>
+           <h1 className='altera'>Alterar dados</h1>
 
                <form onSubmit={handlerLogin}>
 
         <input placeholder='nome' type='nome' className='input'></input>
         <input placeholder='E-mail' type='email' className='input'></input>
         <input placeholder='Senha' type='password' className='input'></input>
-
-        <button class='botaozinho'>Entrar</button>
+        <div className="lugar1">
+        <button class='botaozinho1'><a href="/pages/dashboard">Entrar</a></button>
+        </div>
         </form>
         <ToastContainer/>
         </div>
