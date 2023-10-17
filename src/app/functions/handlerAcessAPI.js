f706cc920adc1b8f1107fad17a4eb132bf74ddce
Betrizav <beatriz.vieira@estudante.ifms.edu.br>
2023-10-17 15:59:26 -0400

    Update handlerAcessAPI.js

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -4,16 +4,23 @@ const url = "https://projetoptac-1rgp.vercel.app";
 
 
 const getUserAuthenticated = async (user) => {
-const responseOfApi = await fetch(url + "/user/authenticate");
-    let userAuth = {};
-
+const responseOfApi = await fetch(url + "/user/authenticate",
+    {
+        method: "POST",
+        head: { "content-type": "application/json"},
+        body: JSON.stringify(user)
+    }
+  );
+  const userAuth = await responseOfApi.json();
+  return userAuth;
+}
 Users.map((user) => {
     if (user.email === userAcess.email & user.senha === userAcess.password){
         userAuth = user
     }
 })
-return userAuth;
-}
+
+
 
 const getUsers = () =>{
         return Users;
