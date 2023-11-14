ecb1e97e96a0373dc0b212a61960ab345145a6b8
Betrizav <beatriz.vieira@estudante.ifms.edu.br>
2023-11-14 10:26:14 -0400

    Update validateToken.js

--- src/app/functions/validateToken.js
+++ src/app/functions/validateToken.js
@@ -1,2 +1,12 @@
 import { jwtVerify } from "jose";
 import { decode } from "jsonwebtoken";
+
+const validadeToken = async(token) => {
+    const secret = ""
+      try{
+        const isTokenValidate = await jwtVerify( token, new TextEncoder().encode(secret))
+      }
+  export { validadeToken };
+ 
+
+}
